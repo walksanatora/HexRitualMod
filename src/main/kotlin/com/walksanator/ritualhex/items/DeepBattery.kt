d2896a8ecf665c0abbea1479fac294fa2cfa297d
walksanatora <walkerffo22@hotmail.com>
2022-08-15 23:26:47 -0700

    get ready for the deep storage battery

---
+++ src/main/kotlin/com/walksanator/ritualhex/items/DeepBattery.kt
@@ -0,0 +1,28 @@
+package com.walksanator.ritualhex.items
+
+import at.petrak.hexcasting.api.item.ManaHolderItem
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+
+class DeepBattery : Item,ManaHolderItem {
+    
+    override fun getMana(items: ItemStack) : Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMaxMana(items: ItemStack?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun setMana(p0: ItemStack?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun manaProvider(p0: ItemStack?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun canRecharge(p0: ItemStack?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
